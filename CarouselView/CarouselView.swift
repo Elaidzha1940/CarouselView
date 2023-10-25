60837f1d1b00a9434b459adeafc3830e23eadb3a
Elaidzha1940 <eli080293@gmail.com>
2023-10-26 01:27:06 +0300

    Update CarouselView

--- CarouselView/CarouselView.swift
+++ CarouselView/CarouselView.swift
@@ -9,20 +9,34 @@
 
 import SwiftUI
 
-struct CarouselView: View {
+struct CarouselView : View {
     @State private var snappedItem = 0.0
-    @State private var dragging = 1.0
+    @State private var draggingItem = 1.0
     @State private var avtiveIndex: Int = 0
     
     var views: [CarouselViewHelper]
+    
+    var xDistance: Int = 150
     
     var body: some View {
         
         ZStack {
             ForEach(views) { view in
-                
+                view
+                    .scaleEffect(1.0 - abs(distance(view.id)) * 0.2)
+                    .opacity(1.0 - abs(distance(view.id)) * 0.3)
+                    .offset(x: getOffset(), y: 0)
             }
         }
+    }
+    
+    func distance(_ item: Int) -> Double {
+        return (draggingItem - Double(item).remainder(dividingBy: Double(views.count)))
+    }
+    
+    func getOffset(_ item: Int) -> Double {
+        let angle = Double.pi * 2 / Double(views.count) * distance(item)
+        return sin(angle) * Double(xDistance)
     }
 }
 

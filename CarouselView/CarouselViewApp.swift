a88c575ad66b3d6690ac55115f029c0b8fd977e8
Elaidzha1940 <eli080293@gmail.com>
2023-10-25 22:38:21 +0300

    CarouselView

---
+++ CarouselView/CarouselViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselViewApp.swift
+//  CarouselView
+//
+//  Created by Elaidzha Shchukin on 25.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CarouselViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
